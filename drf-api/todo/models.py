bf2ce2b93155e3c1a37e85a73d883f914812bc55
Jonathan Falcon A <jona98.falcon@gmail.com>
2024-06-19 23:36:16 -0400

    feat(api): Modelo Todo
    Version 1

--- drf-api/todo/models.py
+++ drf-api/todo/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Todo(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    completed = models.BooleanField(default=False)
+    priority = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.description = models.TextField(blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Tarea'
+        verbose_name_plural = 'Tareas'
+        ordering = [id, 'name']
+
+    def __str__(self):
+        # String representation of the To-Do item, useful in the admin interface and elsewhere
+        return self.name
+
