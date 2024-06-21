d6ad1433c584b6db4aa8fbc3aa3760fef65de62d
Jonathan Falcon A <jona98.falcon@gmail.com>
2024-06-20 22:41:17 -0400

    feat(api): Actualización a ViewSets y filtros en views.py
    Filtros en tasks:
    - priority
    - completed
    Filtros en assigns:
    - user=id
    - task=id

--- drf-api/todo/urls.py
+++ drf-api/todo/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from .views import *
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-    path('tareas/', TaskListCreate.as_view(), name='task-list'),
-    path('tareas/<int:pk>', TaskRetrieveUpdateDestroy.as_view(), name='task-detail'),
-    path('users/', UserListCreate.as_view(), name='user-list'),
-    path('users/<int:pk>', UserRetrieveUpdateDestroy.as_view(), name='user-detail'),
-    path('assings/', AssignListCreate.as_view(), name='assing-list'),
-    path('assings/<int:pk>', AssignRetrieveUpdateDestroy.as_view(), name='assing-detail'),
-]
+router = DefaultRouter()
+router.register('tasks', TaskViewSet, basename = 'tasks')
+router.register('users', UserViewSet, basename = 'users')
+router.register('assigns', AssignViewSet, basename = 'assigns')
+
+urlpatterns = router.urls
+
