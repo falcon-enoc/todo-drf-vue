c1fe63f24b9ff52c7e77d8969841d0acdfad3cca
Enoc Falcón <83475399+falcon-enoc@users.noreply.github.com>
2024-06-23 02:39:00 -0400

    chore(api): cambio de sqlite3 a postgres

--- drf-api/api_todo/settings.py
+++ drf-api/api_todo/settings.py
@@ -11,10 +11,16 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+dotenv_path = BASE_DIR / '.env'
+load_dotenv(dotenv_path)
+
+print(f"Connecting to PostgreSQL database: {os.getenv('POSTGRES_DB')} at {os.getenv('POSTGRES_HOST')} on port {os.getenv('POSTGRES_PORT')}")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -94,8 +100,12 @@ WSGI_APPLICATION = 'api_todo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
 
