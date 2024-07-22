85a331de0bd4c5fb0af911f09deb34cde4663039
Enoc Falcón <83475399+falcon-enoc@users.noreply.github.com>
2024-07-21 23:40:04 -0400

    feat(ui): información de la task en el modal
    tiene un pequeño error que no se ven las tags de la tarea

--- vue-ui/src/components/tasks2.vue
+++ vue-ui/src/components/tasks2.vue
@@ -11,7 +11,25 @@
         <div v-if="isModalOpen" class="modal">
             <div class="modal-content">
                 <span class="close-btn" @click="closeModal">&times;</span>
-                <!-- <p>{{ modalContent }}</p> -->
+                <div class="task-details">
+                    <p><strong>ID:</strong> {{ modalContent.id }}</p>
+                    <p><strong>Title:</strong> {{ modalContent.title }}</p>
+                    <p><strong>Priority:</strong> {{ modalContent.priority }}</p>
+                    <p><strong>Completed:</strong> {{ modalContent.completed }}</p>
+                    <p><strong>Active:</strong> {{ modalContent.active }}</p>
+                    <p><strong>Description:</strong> {{ modalContent.description }}</p>
+                    <p><strong>Created At:</strong> {{ modalContent.created_at }}</p>
+                    <p><strong>Updated At:</strong> {{ modalContent.updated_at }}</p>
+                    <p><strong>Deadline Time:</strong> {{ modalContent.deadline_time }}</p>
+                    <p><strong>Tags:</strong>
+                        <span v-for="tag in modalContent.tags" :key="tag.id" class="tag-badge">{{ tag.tag }}</span>
+                    </p>
+                    <p><strong>Referrer Task:</strong> {{ modalContent.refeer_task?.title }}</p>
+                    <p><strong>Level:</strong> {{ modalContent.level }}</p>
+                    <p><strong>Board:</strong> {{ modalContent.board }}</p>
+                    <p><strong>User:</strong> {{ modalContent.user }}</p>
+                </div>
+
             </div>
         </div>
     </div>
@@ -30,23 +48,41 @@ const props = defineProps({
 });
 
 const isModalOpen = ref(false);
-const modalContent = ref('');
+const modalContent = ref(null);
 const tasks = ref([]);
 const error = ref(null);
 
 const openModal = (task) => {
-    modalContent.value = `ID: ${task.id}\nTitle: ${task.title}`;
+    console.log(task);
+    modalContent.value = { 
+        id: task.id,
+        title: task.title,
+        priority: task.priority,
+        completed: task.completed,
+        active: task.active,
+        description: task.description,
+        created_at: task.created_at,
+        updated_at: task.updated_at,
+        deadline_time: task.deadline_time,
+        tags: task.tags.map(tag => ({ id: tag.id, tag: tag.tag })),
+        refeer_task: task.refeer_task ? { id: task.refeer_task.id, title: task.refeer_task.title } : null,
+        level: task.level,
+        board: task.board,
+        user: task.user
+    };
+    
     isModalOpen.value = true;
 }
 
 const closeModal = () => {
     isModalOpen.value = false;
-    modalContent.value = '';
+    modalContent.value = null;
 }
 
 const loadTasks = async () => {
     try {
         tasks.value = await getTaskByBoardId(props.boardId);
+        console.log(task.value);
         sortTasksById();
     } catch (err) {
         error.value = err;
@@ -64,9 +100,7 @@ onMounted(() => {
 watch(() => props.boardId, (newBoardId) => {
     loadTasks();
 });
-
 </script>
-
 <style scoped>
 .card {
     height: 50px;
@@ -92,19 +126,24 @@ watch(() => props.boardId, (newBoardId) => {
     top: 0;
     width: 100%;
     height: 100%;
-    overflow: auto;
     background-color: rgba(0, 0, 0, 0.4);
 }
 
 .modal-content {
-    background-color: #fefefe;
-    margin: 15% auto;
+    background-color: var(--primary-color);
+    margin: 5% auto; /* Ajusta esta propiedad para cambiar la altura desde la parte superior */
     padding: 20px;
-    border: 1px solid #888;
+    border: 1px solid var(--secondary-color);
     width: 80%;
     max-width: 600px;
+    min-width: 300px;
     border-radius: 5px;
     box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+    color: var(--text-color);
+}
+
+.task-details p {
+    margin: 5px 0;
 }
 
 .close-btn {
