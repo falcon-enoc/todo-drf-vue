cde70e5fa3af2f82c3a71a826bd4f749d12fbfb1
Enoc Falcón <83475399+falcon-enoc@users.noreply.github.com>
2024-07-21 23:39:21 -0400

    refactor(ui): limpieza de componentes inutiles

--- vue-ui/src/views/HomeView.vue
+++ vue-ui/src/views/HomeView.vue
@@ -1,15 +1,10 @@
 <template>
   <div class="main">
-    <!-- <CreateTask /> -->
     <Board />
-    <!-- <Tasks /> -->
   </div>
 </template>
 
 <script setup>
-import MyComponent from "../components/MyComponent.vue";
-// import Tasks from "../components/tasks.vue";
-// import CreateTask from "../components/createTask.vue";
 import Board from "../components/board.vue";
 </script>
 
